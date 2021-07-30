006433590b66335e276b1f078bcfef7101c46709
theVoogie <vugar.abdulzada@webstep.no>
2021-07-30 10:46:40 +0200

    First template

---
+++ public/js/loadProducts.js
@@ -0,0 +1,33 @@
+function createItemFromTemplate(item) {
+  const template = document.querySelector('#product');
+  const product = template.content.cloneNode(true);
+
+  product.querySelector('h2').innerHTML = item.name;
+  product.querySelector('.description').innerHTML = item.description;
+  product.querySelector('[name=sku]').value = item.sku;
+  product.querySelector('.price').innerHTML = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: item.currency
+  }).format((item.amount / 100).toFixed(2));
+  
+  const img = product.querySelector('img');
+  img.src = item.image;
+  img.alt = item.name;
+
+  return product;
+}
+
+export async function loadProducts() {
+  const data = await fetch('/.netlify/functions/getProducts')
+    .then((result) => {
+      return result.json();
+    }).catch((err) => {
+      console.error(err);
+    });
+
+  const products = document.querySelector('.products');
+  data.forEach(item => {
+    const product = createItemFromTemplate(item);
+    products.appendChild(product);
+  });
+}
